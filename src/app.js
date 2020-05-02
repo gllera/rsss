e1e1a7d55ec7c25e536cb4d79cc215d5fa079354
gllera <gabriellleragarcia@gmail.com>
2020-05-02 04:13:29 +0200

    Refactoring

--- src/app.js
+++ src/app.js
@@ -13,20 +13,23 @@ const { configs, parseOPML, init } = require('./utils')
 
 async function appPromise() {
     await init()
-    startFetcher()
 
     let app = express()
+
     app.use(logger('dev'))
     app.use(cors())
-    app.use(fileUpload({ limits: { fileSize: configs.import_upload_max_filesize, files: 1, fields: 0 } }))
+    app.use(fileUpload({
+        limits: {
+            fileSize: configs.import_upload_max_filesize,
+            files: 1,
+            fields: 0
+        }
+    }))
+
 
     if (configs.gui_user)
         app.use(express.static('dist'))
 
-    const schema = makeExecutableSchema({
-        typeDefs: importSchema('shemas_gql/schema.graphql'),
-        resolvers
-    })
 
     app.post('/rsss/import', async (req, res) => {
         if (!req.files || !req.files.file)
@@ -35,12 +38,17 @@ async function appPromise() {
         res.json(await resolvers.Mutation.sourceAddBulk(null, { o: parseOPML(req.files.file) }))
     })
 
-    app.use('/rsss',
-        graphqlHTTP({
-            graphiql: configs.gui_graphql,
-            schema,
-        }))
 
+    app.use('/rsss', graphqlHTTP({
+        graphiql: configs.gui_graphql,
+        schema: makeExecutableSchema({
+            typeDefs: importSchema('schema.graphql'),
+            resolvers
+        })
+    }))
+
+
+    startFetcher()
     return app
 }
 
